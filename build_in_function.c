2786ee77c6d9164c7c50f1091c641e7199a56ebb
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 01:16:50 +0600

    mahabub

---
+++ build_in_function.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+int main()
+{
+    double a;
+    scanf("%lf",&a);
+    //int b=ceil(a);
+    //printf("%d",b);
+    //int b=floor(a);
+    //printf("%d",b);
+    //double b=round(a);
+    //printf("%lf",b);
+    //int b=sqrt(a);
+    //printf("%d",b);
+    //double b=sqrt(a);
+    //printf("%lf",b);
+    double c=abs(a);
+    printf("%lf",c);
+}
