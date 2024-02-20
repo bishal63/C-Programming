06b78def10fef76dd9e1129cc025bb3a417d0c69
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 00:04:11 +0600

    mahabub

---
+++ arithmatic_operator.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int a=30;
+    int b=20;
+    int sum=a+b;
+    int sub=a-b;
+    int mul=a*b;
+    float div=a/b;
+    int mod=a%b;
+    printf("the sum is:");
+    printf("%d",sum);
+    printf("\n");
+    printf("the sub is:");
+    printf("%d",sub);
+    printf("\n");
+    printf("the mul is:");
+    printf("%d",mul);
+    printf("\n");
+    printf("the division is:");
+    printf("%f",div);
+    printf("\n");
+    printf("the mod is:");
+    printf("%d",mod);
+    printf("\n");
+
+
+    return 0;
+}
