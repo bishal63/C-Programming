cd1a928aa53eb85358b40aa7ed4fc4181f6903c1
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 01:26:03 +0600

    mahabub

---
+++ matrix_problem_3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int matrix[5][6];
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<6;j++)
+        {
+            printf("%d%d ",i,j);
+        }
+        printf("\n");
+        
+    }
+}
