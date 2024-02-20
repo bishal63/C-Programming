c54248005f33ca9a5be7774040c16783b677df22
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 01:49:15 +0600

    mahabub

---
+++ unit_matrix.c
@@ -0,0 +1,95 @@
+//primary unit matrix
+/*#include<stdio.h>
+int main()
+{
+    int row,col;
+    scanf("%d %d",&row,&col);
+    int matrix[row][col];
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            scanf("%d",&matrix[i][j]);
+        }
+    }
+    int flag=1;
+    if(row!=col)
+    {
+        flag=0;
+    }
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            if(i==j)
+            {
+                if(matrix[i][j]!=1)
+                {
+                    flag=0;
+                }
+                continue;
+            }
+            if(matrix[i][j]!=0)
+            {
+                flag=0;
+            }
+        }
+        
+    }
+    if(flag==1)
+    {
+        printf("primary unit matrix");
+    }
+    else
+    {
+        printf("not primary unit matrix");
+    }
+}*/
+
+//secondary unit matrix
+#include<stdio.h>
+int main()
+{
+    int row,col;
+    scanf("%d %d",&row,&col);
+    int matrix[row][col];
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            scanf("%d",&matrix[i][j]);
+        }
+    }
+    int flag=1;
+    if(row!=col)
+    {
+        flag=0;
+    }
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            if(i+j==row-1)
+            {
+                if(matrix[i][j]!=1)
+                {
+                    flag=0;
+                }
+                continue;
+            }
+            if(matrix[i][j]!=0)
+            {
+                flag=0;
+            }
+        }
+        
+    }
+    if(flag==1)
+    {
+        printf("secondary unit matrix");
+    }
+    else
+    {
+        printf("not secondary unit matrix");
+    }
+}
