9ddcf50875625859a54ca090335bc252cd9701dc
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 00:32:18 +0600

    mahabub

---
+++ math_build_function.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    //math ceil function
+    double a=4.5;
+    int b=ceil(a);
+    printf("%d",b);
+    return 0;
+}
+
+/*#include<stdio.h>
+#include<math.h>
+int main()
+{
+    //math floor function
+    double a=56.76857;
+    int b=floor(a);
+    printf("%d",b);
+    return 0;
+}*/
+
+/*#include<stdio.h>
+#include<math.h>
+int main()
+{
+    //math round function
+    double a=56.532;
+    double b=round(a);
+    printf("%lf",b);
+    return 0;
+}*/
+
+/*#include<stdio.h>
+#include<math.h>
+int main()
+{
+    //math square function 
+    int a=36;
+    int b=sqrt(a);
+    printf("%d",b);
+    return 0;
+}*/
+
+/*#include<stdio.h>
+#include<math.h>
+int main()
+{
+    //math power function
+    int a=9;
+    int b=3;
+    int c=pow(a,b);
+    printf("%d",c);
+    return 0;
+}*/
+
+/*#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+int main()
+{
+    //math absoulate function
+    int a=-49;
+    int b=abs(a);
+    printf("%d",b);
+    return 0;
+}*/
