0227e4d706dcb4040cbb62c1aa008e630c5d94a1
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 00:47:03 +0600

    mahabub

---
+++ while_loop.c
@@ -0,0 +1,23 @@
+/*#include<stdio.h>
+int main()
+{
+    int a=1;
+    while(a<=10)
+    {
+        printf("I love coding\n");
+        a++;
+    }
+    return 0;
+}*/
+
+#include<stdio.h>
+int main()
+{
+    int a=1;
+    while(a<=10)
+    {
+        printf("%d\n",a);
+        a++;
+    }
+    return 0;
+}
