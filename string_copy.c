1130705bbb7af98222fc6a303aef8d93239045df
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 00:42:19 +0600

    mahabub

---
+++ string_copy.c
@@ -0,0 +1,26 @@
+/*#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[100],b[100];
+    scanf("%s %s",a,b);
+    printf("%s %s",a,b);
+    printf("\n");
+    for(int i=0;i<=strlen(b);i++)
+    {
+        a[i]=b[i];
+    }
+    printf("%s %s",a,b);
+    return 0;
+}*/
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[100],b[100];
+    scanf("%s %s",a,b);
+    strcpy(b,a);
+    printf("%s %s",a,b);
+    return 0;
+}
