d96b40bf3a03a581f30f3f1fd064c356df13d5e9
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 00:37:16 +0600

    mahabub

---
+++ relational_operator.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a=20;
+    int b=30;
+    if(a<b)
+    {
+        printf("true");
+    }
+    else
+    {
+        printf("false");
+    }
+    return 0;
+}
