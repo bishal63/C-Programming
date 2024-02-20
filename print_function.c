662495464b85b3f5470c68e97cf599d68c302edf
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 00:36:05 +0600

    mahabub

---
+++ print_function.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    printf("I am a future programmer of bangladesh.\n");
+    printf("I love programming.\n");
+    printf("My gmail is mdmahabubalambishal@gmail.com\n");
+    printf("100%%\n");   // (%) symbol print korar jonno double %% babohar kora hoy
+    printf("mahabub\\bishal");
+    return 0;
+}
