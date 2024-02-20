7879d2f222cb6715db0a3483bbe43ed2c15e5b47
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 00:31:38 +0600

    mahabub

---
+++ main_program.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    for(int a=1;a<=10;a++)
+    {
+        for(int i=1;i<=5;i++)
+        {
+            printf("%d ",i);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
