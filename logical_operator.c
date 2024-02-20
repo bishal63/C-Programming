fec20a3d678a4b63063bf7f1ba879846a85e90ab
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 00:27:52 +0600

    mahabub

---
+++ logical_operator.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a=20;
+    int b=15;
+    int c=10;
+    if(a>b&&b>c)
+    {
+        printf("true");
+    }
+    else
+    {
+        printf("false");
+    }
+    return 0;
+}
