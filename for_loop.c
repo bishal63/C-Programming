6443e13749999e60d9ddc29de2629122fd9a5ea6
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 00:19:58 +0600

    mahabub

---
+++ for_loop.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=1000;i++)
+    {
+        printf("%d.wellcome to my coding world!!!\n",i);
+        //printf("%d\n",i);
+    }
+    return 0;
+}
+
+/*#include<stdio.h>
+int main()
+{
+    int a;
+    for(a=1;a<=100;a++)
+    {
+        printf("%d",a);
+        printf("\n");
+    }
+    return 0;
+}*/
