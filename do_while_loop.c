63133661f6d7ef788f37178ef05e6280a8d92998
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 00:19:21 +0600

    mahabub

---
+++ do_while_loop.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int a=1;
+    do
+    {
+        printf("%d\n",a);
+        a++;
+    }
+    while(a<=10);
+    
+    return 0;
+}
+
+/*#include<stdio.h>
+int main()
+{
+    int a=0;
+    int i;
+    for(i=0;i<5;i++)
+    {
+        a=a+i;
+        
+    }
+    printf("%d",a);
+    return 0;
+}*/
