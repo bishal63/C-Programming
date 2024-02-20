1871c2cbc8fe78f919216d5517128010a673e1ec
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 00:15:49 +0600

    mahabub

---
+++ array_sum.c
@@ -0,0 +1,28 @@
+/*#include<stdio.h>
+int main()
+{
+    int n;
+    printf("input:");
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+        sum=sum+arr[i];
+
+    }
+    printf("%d ",sum);
+    return 0;
+}*/
+
+#include<stdio.h>
+int main()
+{
+    int arrayName[]={1,2,3};
+    printf("%d",arrayName[3]);
+    return 0;
+}
