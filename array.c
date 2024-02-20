0483eceef5c6a9697d00fac1521657b40fe96eec
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 00:05:41 +0600

    mahabub

---
+++ array.c
@@ -0,0 +1,29 @@
+/*#include<stdio.h>
+int main()
+{
+    int m[5];
+    m[0]=10;
+    m[1]=20;
+    m[2]=30;
+    m[3]=40;
+    m[4]=50;
+    //printf("%d\n",m[0]);
+    for(int i=0;i<5;i++)
+    {
+        printf("%d ",m[i]);
+    }
+    return 0;
+}*/
+
+#include<stdio.h>
+int main()
+{
+    int m[5];
+    m[0]=40;
+    m[1]=20;
+    m[2]=70;
+    m[3]=10;
+    m[4]=60;
+    printf("%d %d %d %d %d",m[0],m[1],m[2],m[3],m[4]);
+    return 0;
+}
